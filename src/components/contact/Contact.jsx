d58945fb08783a747ddab889ac543eaef7cfed42
Victor Cuevas <50884654+VictorCuevas98@users.noreply.github.com>
2024-10-22 21:10:14 -0600

    removing contact information for security reasons

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -33,17 +33,17 @@ const Contact = () => {
           <article className="contact__option">
             <AiOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>axte_vic@hotmail.com</h5>
-            <a href="mailto:axte_vic@hotmail.com" target="_blank">
+            <h5></h5>
+            <a href="" target="_blank">
               Send a message
             </a>
           </article>
           <article className="contact__option">
             <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
-            <h5>+525616226208</h5>
+            <h5></h5>
             <a
-              href="https://api.whatsapp.com/send?phone=5616226208"
+              href=""
               target="_blank"
             >
               Send a message
