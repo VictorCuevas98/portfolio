3abf1946a88d87eb13e0665c75669ee64af43cf1
Victor Cuevas <50884654+VictorCuevas98@users.noreply.github.com>
2024-10-29 14:48:24 -0600

    updating about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,91 +1,90 @@
-import React, { useEffect } from 'react'
-import './about.css'
-import DESKTOP from '../../assets/aboutme/desktop1.jpeg'
-import imagen1 from '../../assets/aboutme/imagen1.jpeg'
-import imagen2 from '../../assets/aboutme/imagen2.jpg'
-import certificate from '../../assets/aboutme/certiface_english.pdf'
-import {FaAward} from 'react-icons/fa'
-import {BiBookBookmark} from 'react-icons/bi'
-import {MdWorkOutline} from 'react-icons/md'
-import { gsap } from "gsap"
-import gsapCore from 'gsap/gsap-core'
-import { useLanguage } from '../../context/LanguageContext'
+import "./about.css";
+import styles from "./About.module.css";
+import image1 from "../../assets/aboutme/image1.jpg";
+import image2 from "../../assets/aboutme/image2.jpg";
+import image3 from "../../assets/aboutme/image3.jpg";
+import certificate from "../../assets/aboutme/certiface_english.pdf";
+import { FaAward } from "react-icons/fa";
+import { BiBookBookmark } from "react-icons/bi";
+import { MdWorkOutline } from "react-icons/md";
+import { useLanguage } from "../../context/LanguageContext";
+import Carousel from "react-bootstrap/Carousel";
 
-const About = () => {  
+const images = [image1, image2, image3];
 
-    const { getTranslation } = useLanguage();
-    // wait until DOM has been rendered  
-    useEffect(() => {    
-        let count = 0;    
-        const targets = document.querySelectorAll(".about__me-image");    
-        gsap.set(targets, { xPercent:100 });    
-        gsap.set(targets[0], { xPercent: 0 });    
-        // next button    
-        function slideOneNext(){      
-            gsap.fromTo(targets[count], { xPercent:0, zIndex:0}, {delay:0.2, duration:1.2, xPercent:0, zIndex:-10});      
-            count = count < targets.length - 1 ? ++count : 0;      
-            gsap.fromTo(targets[count], { xPercent:100, zIndex:10}, {duration:1.2, xPercent:0, zIndex:0});    
-        }    
-        const nextBtn = document.getElementById("nextButton");    
-        nextBtn.addEventListener("click", function(){      
-            slideOneNext();    
-        });    
-        // Previous button    
-        function slideOnePrev() {      
-            gsap.fromTo(targets[count], { xPercent: 0, zIndex: 10 }, {xPercent: 0, zIndex: 0 });      
-            gsap.fromTo(targets[count], { xPercent: 0, zIndex: 0 }, { delay: 0, duration: 1.2, xPercent: 100, zIndex: -10 });      
-            count = count < targets.length ? --count : 0;    
-        }    
-        const prevBtn = document.querySelector("#prevButton");    
-        prevBtn.addEventListener("click", function(){      
-            slideOnePrev();    
-        });           
-    });  
-    return (    
-        <section id='about'>      
-            <h5>{getTranslation("gtk")}</h5>      
-            <h2>{getTranslation("am")}</h2>      
-            <div className="container about__container">        
-                <dir className="about__me">          
-                    <div className='about__me-image'>            
-                        <img src={DESKTOP} alt="" />          
-                    </div>          
-                    <div className='about__me-image'>            
-                        <img src={imagen1} alt="" />          
-                    </div>          
-                    <div className='about__me-image'>            
-                        <img src={imagen2} alt="" />          
-                    </div>                    
-                    <div className='controls'>            
-                        <span id='prevButton'>Prev</span>            
-                        <span id='nextButton'>Next</span>          
-                    </div>        
-                </dir>                
-                <div className='about__content'>          
-                    <div className="about__cards">            
-                        <article className='about__card'>              
-                            <BiBookBookmark className='about__icon'/>              
-                            <h5>Education</h5>              
-                            <small>Bechalor's Computer Systems</small>            
-                        </article>            
-                        <article className='about__card'>              
-                            <MdWorkOutline className='about__icon'/>              
-                            <h5>Experience</h5>              
-                            <small>4+ experience years</small>            
-                        </article>            
-                        <article className='about__card'>              
-                            <FaAward className='about__icon'/>              
-                            <h5>English Advance</h5>              
-                            <small><a className='link' href={certificate} target="__blank">Certificate of Level</a></small>            
-                        </article>          
-                    </div>          
-                    <p>            
-                        Developer full time, excited about tecnology and passionate to create new projects chanllenging.             
-                        I'm always find myself learning something by my own, enjoying the proccess of improvment.            
-                    </p>                    
-                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>        
-                </div>      
-            </div>    
-        </section>  
-    )}
-    export default About
+const About = () => {
+  const { getTranslation } = useLanguage();
+
+  return (
+    <section id="about">
+      <h5>{getTranslation("gtk")}</h5>
+      <h2>{getTranslation("am")}</h2>
+      <div className="container about__container">
+        <dir className="about__me">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src={image1}
+                alt=""
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src={image2}
+                alt=""
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src={image3}
+                alt=""
+              />
+            </Carousel.Item>
+          </Carousel>
+          {/* <div className='about__me-image'>               
+                        {images.map((image, index) => (
+                            <img key={index} src={image} alt={`Slide ${index + 1}`} />        
+                        ))}       
+                    </div>                           */}
+        </dir>
+        <div className="about__content">
+          <div>
+            <div className="about__cards">
+              <article className="about__card">
+                <BiBookBookmark className="about__icon" />
+                <h5>Education</h5>
+                <small>Bechalor's Computer Systems</small>
+              </article>
+              <article className="about__card">
+                <MdWorkOutline className="about__icon" />
+                <h5>Experience</h5>
+                <small>4+ experience years</small>
+              </article>
+              <article className="about__card">
+                <FaAward className="about__icon" />
+                <h5>English Advance</h5>
+                <small>
+                  <a className="link" href={certificate} target="__blank">
+                    Certificate of Level
+                  </a>
+                </small>
+              </article>
+            </div>
+            <p>
+              Developer full time, excited about tecnology and passionate to
+              create new projects chanllenging. I'm always find myself learning
+              something by my own, enjoying the proccess of improvment.
+            </p>
+            <a href="#contact" className="btn btn-primary">
+              Let's Talk
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
