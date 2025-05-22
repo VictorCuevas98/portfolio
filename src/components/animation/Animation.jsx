e245689612fe85e10899e35c399386243385dcb8
Victor Cuevas <50884654+VictorCuevas98@users.noreply.github.com>
2025-05-23 08:15:26 +0900

    change cv

--- src/components/animation/Animation.jsx
+++ src/components/animation/Animation.jsx
@@ -20,66 +20,76 @@ const Animation = () => {
     }
     var x = window.matchMedia("(max-width: 800px)");
     myFunction(x);
-    
+
     // GSAP animation
-    gsap.registerPlugin(DrawSVGPlugin) 
+    gsap.registerPlugin(DrawSVGPlugin);
     gsap.registerPlugin(TextPlugin);
     //this is to show the pen
-    
+
     // gsap.set(".cls-1", { visibility: "visible" });
     // gsap.set(".cls-2", { visibility: "visible" });
     //  gsap.set("#svg1, #svg2", { visibility: "visible" });
 
     //Drawing a pen
-    var tl1 = gsap.timeline({
+    var tl1 = gsap
+      .timeline({
         //repeat: -1,
-        defaults:{ duration: 0.70, ease: 'power1.inOut' }
-    })          
-    .from('#cls-2-5', {stagger: 0.1, drawSVG: 0 })
+        defaults: { duration: 0.3, ease: "power1.inOut" },
+      })
+      .from("#cls-2-5", { stagger: 0.1, drawSVG: 0 })
 
-    .from('#cls-2-2', {stagger: 0.1, drawSVG: 0 })
-    .from('#cls-2-3', {stagger: 0.1, drawSVG: 0 })
-    
-    .from('#cls-2-6', {stagger: 0.1, drawSVG: 0 })
-    
-    .from('#cls-2-1', {stagger: 0.1, drawSVG: 0 })
-    .fromTo('#cls-1-1', {stagger: 0.1, drawSVG: "100% 100%" }, {drawSVG: "100% 0"})
-    .from('#cls-2-4', {stagger: 0.1, drawSVG: 0 })
-    .from('#cls-1-2', {stagger: 0.1, drawSVG: 0 })
-    
-    .from('#cls-1-3', {stagger: 0.1, drawSVG: 0 })    
-    .from('#cls-1-4', {stagger: 0.1, drawSVG: 0 })    
-    .from('#cls-1-5', {stagger: 0.1, drawSVG: 0 })    
+      .from("#cls-2-2", { stagger: 0.1, drawSVG: 0 })
+      .from("#cls-2-3", { stagger: 0.1, drawSVG: 0 })
 
-    
-    /* Opening the pen */
-    .to(".pen2", {
-      duration: 3,
-      x: 1000,
-      opacity: 0,
-      ease: "slow (0.3, 2, false)",
-    })
-    .to(".pen1", {
-      duration: 3,
-      x: -1000, 
-      opacity: 0, 
-      ease: "slow (0.1, 0.1, false)"},"<") // "<" means start at the same time as previous
+      .from("#cls-2-6", { stagger: 0.1, drawSVG: 0 })
 
-    /* Showing the website*/
-    .to(".text1", { duration: 1, text: "|" }, "-=3") 
-    .to(".text1", { duration: 0, text: "" })
-    .to(".text2", { duration: 2, text: "|" })
-    .to(".text2", { duration: 0, text: "" })
-    .to(".text1", { duration: 1, text: "こんにちは"})
-    .to(".text2", { duration: 1, text: "私は ビクターです。"})
-    .to(".container__animation", { position: "absolute"})
+      .from("#cls-2-1", { stagger: 0.1, drawSVG: 0 })
+      .fromTo(
+        "#cls-1-1",
+        { stagger: 0.1, drawSVG: "100% 100%" },
+        { drawSVG: "100% 0" }
+      )
+      .from("#cls-2-4", { stagger: 0.1, drawSVG: 0 })
+      .from("#cls-1-2", { stagger: 0.1, drawSVG: 0 })
 
-    // Animation that shows the rest of the website (cortain going up)
-    .to(".container__animation", { duration: 2, height: "auto", delay: 1 })
-    .to("#root", { position: "relative"});
+      .from("#cls-1-3", { stagger: 0.1, drawSVG: 0 })
+      .from("#cls-1-4", { stagger: 0.1, drawSVG: 0 })
+      .from("#cls-1-5", { stagger: 0.1, drawSVG: 0 })
 
-    // Words animation 
-    var tl2 = gsap.timeline({ repeat: -1, repeatDelay: 2})
+      /* Opening the pen */
+      .to(".pen2", {
+        duration: 2,
+        x: 1000,
+        opacity: 0,
+        ease: "slow (0.3, 2, false)",
+      })
+      .to(
+        ".pen1",
+        {
+          duration: 2,
+          x: -1000,
+          opacity: 0,
+          ease: "slow (0.1, 0.1, false)",
+        },
+        "<"
+      ) // "<" means start at the same time as previous
+
+      /* Showing the website*/
+      .to(".text1", { duration: 0.5, text: "|" }, "-=2")
+      .to(".text1", { duration: 0, text: "" })
+      .to(".text2", { duration: 0.5, text: "|" })
+      .to(".text2", { duration: 0, text: "" })
+      .to(".text1", { duration: 1, text: "こんにちは" })
+      .to(".text2", { duration: 1, text: "私は ビクターです。" })
+      .to(".container__animation", { position: "absolute" })
+
+      // Animation that shows the rest of the website (cortain going up)
+      // .to(".container__animation", { duration: 2, height: "auto", delay: 1 })
+      .to("#root", { position: "relative" });
+
+    // Words animation
+    var tl2 = gsap
+      .timeline({ repeat: -1, repeatDelay: 2 })
       .to(".text1", { duration: 2, text: "|" })
       .to(".text1", { duration: 0, text: "" })
       .to(".text2", { duration: 2, text: "|" })
@@ -106,10 +116,10 @@ const Animation = () => {
       .to(".text2", { duration: 2, text: "私は ビクター です。" });
     // Main timeline that runs them in sequence
     var master = gsap.timeline();
-        master.add(tl1)      // tl1 runs first
-              .add(tl2);     // tl2 starts after tl1 finishes
-
-    }, []);
+    master
+      .add(tl1) // tl1 runs first
+      .add(tl2); // tl2 starts after tl1 finishes
+  }, []);
 
   return (
     <div className="container__animation">
@@ -123,27 +133,32 @@ const Animation = () => {
           >
             <title>pluma punta</title>
             <path
-              className="cls-1" id="cls-1-1"
+              className="cls-1"
+              id="cls-1-1"
               d="M114,31.51l-37.25.63C58.5,32.76,44,33.51,44,33.51v15C56.79,49.17,76,50,93.21,50.26l20.75.25"
               transform="translate(-7.35 -31.01)"
             />
             <path
-              className="cls-1" id="cls-1-2"
+              className="cls-1"
+              id="cls-1-2"
               d="M113.93,37.51h-66v-2l66.1-.73"
               transform="translate(-7.35 -31.01)"
             />
             <path
-              className="cls-1" id="cls-1-3"
+              className="cls-1"
+              id="cls-1-3"
               d="M42,33.51v15s-26-2-29-6v-3S18,35.51,42,33.51Z"
               transform="translate(-7.35 -31.01)"
             />
             <path
-              className="cls-1" id="cls-1-4"
+              className="cls-1"
+              id="cls-1-4"
               d="M16,41.51H40v5s-16,0-24-4Z"
               transform="translate(-7.35 -31.01)"
             />
             <path
-              className="cls-1" id="cls-1-5"
+              className="cls-1"
+              id="cls-1-5"
               d="M11,39.51V42S4,41.18,11,39.51Z"
               transform="translate(-7.35 -31.01)"
             />
@@ -158,32 +173,38 @@ const Animation = () => {
           >
             <title>Pluma 2</title>
             <path
-              className="cls-2" id="cls-2-1"
+              className="cls-2"
+              id="cls-2-1"
               d="M1,31.51H19v19H0"
               transform="translate(0.04 -18.98)"
             />
             <path
-              className="cls-2" id="cls-2-2"
+              className="cls-2"
+              id="cls-2-2"
               d="M21,31.51v19H91a2.94,2.94,0,0,0,3-2.88V34.51s0-3-4-3Z"
               transform="translate(0.04 -18.98)"
             />
             <path
-              className="cls-2" id="cls-2-3"
+              className="cls-2"
+              id="cls-2-3"
               d="M23,34.51v3H90s1-3-2-3Z"
               transform="translate(0.04 -18.98)"
             />
             <path
-              className="cls-2" id="cls-2-4"
+              className="cls-2"
+              id="cls-2-4"
               d="M.5,34.72,17,34.51v3H.23"
               transform="translate(0.04 -18.98)"
             />
             <path
-              className="cls-2" id="cls-2-5"
+              className="cls-2"
+              id="cls-2-5"
               d="M96,35.51v12h8s2,0,2-3v-7a2,2,0,0,0-2-2Z"
               transform="translate(0.04 -18.98)"
             />
             <path
-              className="cls-2" id="cls-2-6"
+              className="cls-2"
+              id="cls-2-6"
               d="M78,29.51s4-5-1-5-39,1-39,1-2,0-1,4c0,0-6,0-6-3,0,0-1-2,5-3s42-4,42-4,9-1,12,10Z"
               transform="translate(0.04 -18.98)"
             />
